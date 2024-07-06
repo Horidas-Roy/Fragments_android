fb5944cbe216fa2dfd112491627efa0e90928a45
Horidas Roy <royhoridas64@gmail.com>
2024-07-06 23:41:34 +0600

    fragment create and implement on host

--- app/src/main/java/com/horidas/fragments/MainActivity.java
+++ app/src/main/java/com/horidas/fragments/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        FragmentManager fManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fManager.beginTransaction();
+        fragmentTransaction.add(R.id.host,new FirstFragment());
+
+        fragmentTransaction.commit();
+
     }
 }
