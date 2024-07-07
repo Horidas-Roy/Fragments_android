059cc1c5785bcfdc3c514658230b828256462a0e
Horidas Roy <royhoridas64@gmail.com>
2024-07-07 11:17:52 +0600

    work with fragment

--- app/src/main/java/com/horidas/fragments/MainActivity.java
+++ app/src/main/java/com/horidas/fragments/MainActivity.java
@@ -1,8 +1,10 @@
 package com.horidas.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,40 +14,95 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button fragment1,fragment2;
+//    Button fragment1,fragment2;
+    TabLayout tabLayout;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        fragment1 = findViewById(R.id.fragment1);
-        fragment2= findViewById(R.id.fragment2);
+        tabLayout = findViewById(R.id.tabLayout);
 
-        fragment1.setOnClickListener(new View.OnClickListener() {
+        FragmentManager fManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fManager.beginTransaction();
+        fragmentTransaction.add(R.id.host,new FirstFragment());
+        fragmentTransaction.commit();
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View v) {
-                FragmentManager fManager = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fManager.beginTransaction();
-                fragmentTransaction.add(R.id.host,new FirstFragment());
+            public void onTabSelected(TabLayout.Tab tab) {
+                 int tabPosition = tab.getPosition();
+                 String tabText = tab.getText().toString();
+                Toast.makeText(MainActivity.this,tabText,Toast.LENGTH_LONG).show();
 
-                fragmentTransaction.commit();
+                    if(tabPosition==0){
+                            FragmentManager fManager = getSupportFragmentManager();
+                            FragmentTransaction fragmentTransaction = fManager.beginTransaction();
+                            fragmentTransaction.add(R.id.host,new FirstFragment());
+                            fragmentTransaction.commit();
+                    } else if (tabPosition==1) {
+                            FragmentManager fManager = getSupportFragmentManager();
+                            FragmentTransaction fragmentTransaction = fManager.beginTransaction();
+                            fragmentTransaction.add(R.id.host,new SecondFragment());
+                            fragmentTransaction.commit();
+                    } else if (tabPosition==2) {
+                            FragmentManager fManager = getSupportFragmentManager();
+                            FragmentTransaction fragmentTransaction = fManager.beginTransaction();
+                            fragmentTransaction.add(R.id.host,new FirstFragment());
+                            fragmentTransaction.commit();
+                    }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
 
-        fragment2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fManager = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fManager.beginTransaction();
-                fragmentTransaction.add(R.id.host,new SecondFragment());
 
-                fragmentTransaction.commit();
-            }
-        });
+
+
+
+
+
+
+  //-----------fragment 1 & 2 --------------
+//        fragment1 = findViewById(R.id.fragment1);
+//        fragment2= findViewById(R.id.fragment2);
+//
+//        fragment1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FragmentManager fManager = getSupportFragmentManager();
+//                FragmentTransaction fragmentTransaction = fManager.beginTransaction();
+//                fragmentTransaction.add(R.id.host,new FirstFragment());
+//
+//                fragmentTransaction.commit();
+//            }
+//        });
+//
+//        fragment2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FragmentManager fManager = getSupportFragmentManager();
+//                FragmentTransaction fragmentTransaction = fManager.beginTransaction();
+//                fragmentTransaction.add(R.id.host,new SecondFragment());
+//
+//                fragmentTransaction.commit();
+//            }
+//        });
 
     }
 }
